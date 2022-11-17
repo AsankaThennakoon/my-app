f7e3916d303892e8fbb40701c6af9de08c3be816
Asanka Thennakoon <asanka.thennakoon@arimaclanka.com>
2022-11-17 09:53:52 +0530

    splitting component complete

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,15 @@
+import "./ExpenseItem.css";
+import "./ExpenseDate";
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+function ExpenseItem(props) {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className='expense-item__description'>{props.title}</div>
+      <div className='expense-item__price'>${props.amount}</div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
---
+++ src/components/UI/Card.js
@@ -0,0 +1,8 @@
+import "./Card.css";
+const Card = (props) => {
+  const classNames = "card " + props.className;
+
+  return <div className={classNames}>{props.children}</div>;
+};
+
+export default Card;
