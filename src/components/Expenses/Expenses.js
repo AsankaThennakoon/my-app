0ecfcd1bbcad5883c8e0fac264aa3571096017e4
Asanka Thennakoon <newsunflowers98@gmail.com>
2022-12-01 09:06:30 +0530

    conditional filter content finsh

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,24 +18,45 @@ function Expenses(props) {
   }
 
   
-const filteredExpense= props.expenses.filter(expense=>expense.date.getFullYear()==filteredYear); 
+const filteredExpense= props.expenses.filter(expense=>expense.date.getFullYear().toString()===filteredYear); 
 
+
+let expenseContent=<p>No Content!</p>
+if(filteredExpense.length>0){
+  expenseContent=filteredExpense.map(expensesItem=><ExpenseItem
+    key={expensesItem.id}
+      title={expensesItem.title}
+      amount={expensesItem.amount}
+      date={expensesItem.date}
+    />);
+}
   return (
-
-   
 
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onAddFilter={onAddFilterHandler}></ExpensesFilter>
-    {
-    
-     filteredExpense.map(expensesItem=><ExpenseItem
-        key={expensesItem.id}
-          title={expensesItem.title}
-          amount={expensesItem.amount}
-          date={expensesItem.date}
-        />)
-    
-    }
+        {/* {
+          filteredExpense.length===0?<p>There is no content</p>: filteredExpense.map(expensesItem=><ExpenseItem
+            key={expensesItem.id}
+              title={expensesItem.title}
+              amount={expensesItem.amount}
+              date={expensesItem.date}
+            />)
+        } */}
+        {
+          // filteredExpense.length===0 && <p>NO content</p>
+        }
+        {
+          // filteredExpense.length>0 && filteredExpense.map(expensesItem=><ExpenseItem
+          //   key={expensesItem.id}
+          //     title={expensesItem.title}
+          //     amount={expensesItem.amount}
+          //     date={expensesItem.date}
+          //   />)
+        }
+        {
+          expenseContent
+        }
+   
   </Card>
 
     
